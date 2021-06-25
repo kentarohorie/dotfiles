7f3e80eea2c950e1b1b95a59cbab83ae833242c5
kentarohorie <kentaro.horie.4@gmail.com>
2021-06-25 21:16:50 +0900

    Set inputrc

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ ln -s ~/dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/screenrc ~/.screenrc
 ln -s ~/dotfiles/.agignore ~/.agignore
+ln -s ~/dotfiles/bash/.inputrc ~/.inputrc
 
 # Install packages for Mac
 # brew install fd
